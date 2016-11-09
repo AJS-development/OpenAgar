e3de09ab019f55880ae9e0e3485f8c2faa921e45
Andrew S <andrews54757@gmail.com>
2016-11-09 13:37:46 -0500

    Update

---
+++ source/commands/lib/list.js
@@ -0,0 +1,32 @@
+"use strict"
+module.exports = function(str,main,log) {
+    var fill = function(a,num,char) {
+        char = char || " "
+        num -= a.length
+        for (var i = 0; i < num; i ++) {
+            a += char
+        }
+        return a
+    }
+    str = str.split(" ")
+    if (str[1] == "players") {
+        // 50
+        log("-----------------------------------Players-----------------------------------")
+        log("| Id  | Ip        | Name          | ChatName        | rank | posX | posY |")
+        main.clients.forEach((client)=>{
+        
+
+            var name = client.gameData.name
+            name = name || "An Unamed Cell";
+          log("|" + fill(client.id.toString(),5) + "|" + fill(client.socket.remoteAddress,10) + "|" + fill(name,15) + "|")  
+        })
+        
+    } else if (str[1] == "bots") {
+        
+    } else if (str[1] == "minions") {
+        
+    } else if (str[1] == "help") {
+        
+    }
+    
+}
