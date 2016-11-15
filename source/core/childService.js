4720c2685730586bfe197363be5b8d545d016c8d
Andrew S <andrews54757@gmail.com>
2016-11-14 20:46:28 -0500

    Update childService.js

--- source/core/childService.js
+++ source/core/childService.js
@@ -1,1 +1,12 @@
-
+"use strict";
+var Child = require('child_process')
+module.exports = class childService {
+  constructor() {
+    this.cpus = require('os').cpus().length
+  }
+  
+  
+  
+  
+  
+}
