2fa1cdc9f1ee1cd08e55266e1f3f6b538f14d188
Andrew S <andrews54757@gmail.com>
2016-11-14 21:10:45 -0500

    Child process preparedness

--- source/core/childService.js
+++ source/core/childService.js
@@ -3,8 +3,14 @@ var Child = require('child_process')
 module.exports = class childService {
   constructor() {
     this.cpus = require('os').cpus().length
+    this.children = [];
+    this.init()
   }
-  
+  init() {
+      for (var i = 0; i < this.cpus; i ++) {
+          Child.fork(__dirname + '/../child/index.js')
+      }
+  }
   
   
   
