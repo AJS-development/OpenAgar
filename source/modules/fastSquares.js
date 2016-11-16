db2281a4e9e29db5282d4d8c342a435ea4fb10e6
Andrew S <andrews54757@gmail.com>
2016-11-15 20:59:51 -0500

    Update fastSquares.js

--- source/modules/fastSquares.js
+++ source/modules/fastSquares.js
@@ -1,6 +1,23 @@
 "use strict"
+var fs = require('fs')
 module.exports = class FastSquares {
  constructor() {
-   
+   this.lib = [];
+  this.init()
+ }
+ init() {
+  try {
+ this.lib = JSON.parse(fs.readFileSync(__dirname + "/squares.json","utf8"))
+  } catch (e) {
+ 
+for (var i = 0; i < 10000000; i ++) {
+ this.lib[i] = Math.sqrt(i)
+}
+   fs.writeFileSync(__dirname + './squares.json',JSON.stringify(this.lib))
+   console.log("Generated library")
+  }
+   sqrt(a) {
+    return this.lib[~~a]
+   }
  }
 }
