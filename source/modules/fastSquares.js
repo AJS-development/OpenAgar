26f69b0310219b015a57539a3e84b726e0b74014
Andrew S <andrews54757@gmail.com>
2016-11-15 21:12:50 -0500

    Faster squaring

--- source/modules/fastSquares.js
+++ source/modules/fastSquares.js
@@ -6,16 +6,13 @@ module.exports = class FastSquares {
   this.init()
  }
  init() {
-  try {
- this.lib = JSON.parse(fs.readFileSync(__dirname + "/squares.json","utf8"))
-  } catch (e) {
+  
  
 for (var i = 0; i < 10000000; i ++) {
  this.lib[i] = Math.sqrt(i)
 }
-   fs.writeFileSync(__dirname + '/squares.json',JSON.stringify(this.lib))
-   console.log("Generated library")
-  }
+  
+  
  }
    sqrt(a) {
     return this.lib[~~a]
