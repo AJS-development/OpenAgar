09814098ca983bad8484d3021f06e0baae19ac5a
Andrew S <andrews54757@gmail.com>
2016-11-18 00:00:47 -0500

    Update fastSquares.js

--- source/modules/fastSquares.js
+++ source/modules/fastSquares.js
@@ -1,5 +1,19 @@
 // No license here! This is free to the public!
 
+// Use: A faster alternative to Math.sqrt()
+
+/*
+How to use:
+
+var a = require('path to fastsquares')
+Sqrt = new a()
+
+Then you can use this anywhere in your code by doing 
+
+Sqrt.sqrt(num)
+
+*/
+
 "use strict"
 
 module.exports = class FastSquares {
