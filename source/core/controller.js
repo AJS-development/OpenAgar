a1885147ab8262664fb1536e3cbeee944d57c563
Andrew S <andrews54757@gmail.com>
2016-11-23 10:06:28 -0500

    Update controller.js

--- source/core/controller.js
+++ source/core/controller.js
@@ -23,7 +23,7 @@ var UID = require('./uid.js');
 Util = require('./utilities.js')
 var a = require('../modules/fastSquares.js')
 Sqrt = new a()
-_version = "2.9.5"
+_version = "3.0.0"
 _key = ""
 try {
     try {_key = parseInt(require('fs').readFileSync(__dirname + '/../../key.txt',"utf8"))} catch(j){
