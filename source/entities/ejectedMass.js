dcfc7b6af0e8038ed0e978558c6815444094235b
Andrew S <andrews54757@gmail.com>
2016-11-27 16:32:01 -0500

    Other copy notices

--- source/entities/ejectedMass.js
+++ source/entities/ejectedMass.js
@@ -1,4 +1,21 @@
 "use strict";
+/*
+    OpenAgar - Open source web game
+    Copyright (C) 2016 Andrew S
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 var template = require('./template.js');
 module.exports = class EjectedMass extends template {
     constructor(position, mass, type, owner, other) {
