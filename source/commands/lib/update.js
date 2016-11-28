2276f8a54a03ffd08d06d77d468f9aaf6523a206
Andrew S <andrews54757@gmail.com>
2016-11-28 17:13:13 -0500

    Update update.js

--- source/commands/lib/update.js
+++ source/commands/lib/update.js
@@ -1,5 +1,13 @@
 module.exports = function(str,ss,log) {
-    ss.updater.update()
+    if (ss._dwe) ss.updater.update(); else {
+     ss._dwe = true;
+        setTimeout(function() {
+            ss._dwe = false;
+        },5000)
+        log("Are you sure you want to update? Enter command again to proceed.")   
+        
+        
+    }
     
     
 }
