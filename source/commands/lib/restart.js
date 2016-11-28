7beeb83529ea838cc61ec502eaf7fe80c80395ce
Andrew S <andrews54757@gmail.com>
2016-11-28 17:17:40 -0500

    Update restart.js

--- source/commands/lib/restart.js
+++ source/commands/lib/restart.js
@@ -1,7 +1,16 @@
 module.exports = function(str,ss,log) {
   
    str = str.split(" ")
+  
   if (str[1] == "all") {
+  if (str[2] == 0 || str[2] == "now") {
+    
+  }
+    
+  
+  } else {
+    
+    
     
   }
   
