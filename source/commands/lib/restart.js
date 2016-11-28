70de385481e8d54e376444d3217c0da4638dd1a8
Andrew S <andrews54757@gmail.com>
2016-11-28 17:20:21 -0500

    Update restart.js

--- source/commands/lib/restart.js
+++ source/commands/lib/restart.js
@@ -3,13 +3,24 @@ module.exports = function(str,ss,log) {
    str = str.split(" ")
   
   if (str[1] == "all") {
-  if (str[2] == 0 || str[2] == "now") {
-    
+    var time = parseInt(str[2])
+  if (str[2] == "now" || time == 0) {
+    ss.restartAll()
   }
-    
+    if (isNaN(time)) return log("Please specify time, in minutes, or type 'restart now' to restart now")
+    setTimeout(function() {
+    ss.restartAll()
+    },60000 * time)
   
   } else {
-    
+       var time = parseInt(str[2])
+  if (str[2] == "now" || time == 0) {
+    ss.restartSelected()
+  }
+    if (isNaN(time)) return log("Please specify time, in minutes, or type 'restart now' to restart now")
+    setTimeout(function() {
+    ss.restartSelected()
+    },60000 * time)
     
     
   }
