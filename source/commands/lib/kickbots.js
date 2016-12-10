dd2b8850efdc275cb2912589c738677930bbdf91
Andrew S <andrews54757@gmail.com>
2016-12-10 17:48:44 -0500

    Update kickbots.js

--- source/commands/lib/kickbots.js
+++ source/commands/lib/kickbots.js
@@ -1,1 +1,13 @@
-
+module.exports = function(str,main,log) {
+  str = str.split(" ");
+  var count = parseInt(str[1])
+  if (isNaN(count)) count = -1;
+  var kicked = 0;
+  main.bots.every(function(bot) {
+    main.removeBot(bot)
+    kicked ++;
+    if (kicked >= count && count > 0) return false;
+    return true;
+  })
+  log("gre{[OpenAgar]} Kicked ".styleMe() + kicked + " bots")
+}
