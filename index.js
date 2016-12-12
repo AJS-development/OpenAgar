894225be7e96466b4a1dfcadcbd518273dffdfb8
Andrew S <andrews54757@gmail.com>
2016-12-11 21:36:25 -0500

    Polyfill

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var main = require('./source/core/controller.js')
-// require('./cpu.js').init('./data')
-// require('./heap.js').init('./data')
+    // require('./cpu.js').init('./data')
+    // require('./heap.js').init('./data')
+Map.prototype.toArray = function () {
+    var array = [];
+    this.forEach(function (a) {
+        array.push(a)
+    })
+    return array
+}
 var m = new main()
 m.start()
