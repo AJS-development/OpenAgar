8f8fa2ef5124139e8141d28fa22b92987ef65904
Andrew S <andrews54757@gmail.com>
2016-12-14 13:10:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var main = require('./source/core/controller.js')
     // require('./cpu.js').init('./data')
     // require('./heap.js').init('./data')
+Map.prototype.every = function (c) {
+    var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        if (!c(b[1], b[0])) return true;
+    }
+  return false;
+}
 Map.prototype.toArray = function () {
     var array = [];
     this.forEach(function (a) {
@@ -8,5 +16,26 @@ Map.prototype.toArray = function () {
     })
     return array
 }
+Map.prototype.map = function(c) {
+  var f = new Map();
+ var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        f.set(b[0],c(b[1], b[0])) 
+    }
+  return f;
+  
+}
+Map.prototype.filter = function(c) {
+  var f = new Map();
+ var a = this.entries()
+    var b;
+    while (b = a.next().value) {
+        if (c(b[1], b[0])) f.set(b[0],b[1])
+    }
+  return f;
+  
+}
+
 var m = new main()
 m.start()
