a2eeb6b3267e84a10c73ded5d3e031b9a81adeec
Andrew S <andrews54757@gmail.com>
2016-12-26 10:30:35 -0500

    Update Teams.js

--- source/modes/Teams.js
+++ source/modes/Teams.js
@@ -1,1 +1,12 @@
-
+"use strict"
+var template = require('./template.js');
+module.exports = class FFA extends template {
+ constructor() {
+     super()
+      this.id = 1;
+    this.name = "TEAMS";
+   
+ }   
+    
+    
+}
