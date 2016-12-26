c637d44a25251b8487d8956246e086b521f6e5d2
Andrew S <andrews54757@gmail.com>
2016-12-26 10:31:20 -0500

    Create Minions.js

---
+++ source/modes/Minions.js
@@ -0,0 +1,12 @@
+"use strict"
+var template = require('./template.js');
+module.exports = class FFA extends template {
+ constructor() {
+     super()
+      this.id = 4;
+    this.name = "Minions";
+   
+ }   
+    
+    
+}
