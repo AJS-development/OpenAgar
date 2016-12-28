7795ee59e2dbe23d1f8e36c66015fb29378d3706
Andrew S <andrews54757@gmail.com>
2016-12-27 21:03:23 -0500

    Update Experimental.js

--- source/modes/Experimental.js
+++ source/modes/Experimental.js
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-var Virus = require('../entities/Virus.js');
+
 var template = require('./template.js');
 module.exports = class Experimental extends template {
     constructor() {
