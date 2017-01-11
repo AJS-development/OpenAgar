4e382c78d880d80889cee39392a586947a826f60
Andrew S <andrews54757@gmail.com>
2017-01-11 18:13:46 -0500

    Update PlayerTemplate.js

--- source/core/PlayerTemplate.js
+++ source/core/PlayerTemplate.js
@@ -1,1 +1,146 @@
+"use strict"
+/*
+    OpenAgar - Open source web game
+    Copyright (C) 2016 Andrew S
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+module.exports = class PlayerTemplate {
+    constructor(id, server, globalData) {
+        this.id = id;
+
+        this.server = server;
+
+        this.mouse = {
+            x: 0,
+            y: 0
+        }
+        this.owning = new Map();
+        this.mass = 0;
+        this.frozen = false;
+        this.score = 0;
+        this.golden = false;
+        this.gameData = {
+            name: "",
+            color: server.getRandomColor(),
+            chatColor: server.getRandomColor(),
+            reservedChatNames: [],
+            chatName: "",
+            chkDeath: false,
+            chatBan: false,
+            reservedNamesMap: []
+        }
+
+        this.bulletsleft = 3
+        this.killer = false;
+        this.globalData = globalData
+      
+        this.minions = new Map()
+        this.cells = new Map();
+        this.server.addClient(this)
+        this.sendData = true;
+
+        this.alive = Date.now()
+
+    }
+    setOwn(node) {
+        this.owning.set(node.id, node)
+    }
+    removeOwn(node) {
+        this.owning.delete(node.id)
+    }
+    addMinion(minion) {
+        this.minions.set(minion.id, minion)
+
+    }
+   
+    setMass(m) {
+        this.cells.forEach((cell) => {
+            cell.updateMass(m)
+        })
+    }
+    removeMinion(minion) {
+        this.minions.delete(minion.id)
+    }
+    reset() {
+        this.minions.clear();
+        this.cells.clear();
+        this.visible = [];
+        this.gameData = {
+            name: "",
+            color: this.server.getRandomColor(),
+            chatColor: this.server.getRandomColor(),
+            reservedChatNames: [],
+            chatName: "",
+            chkDeath: false,
+            chatBan: false,
+            reservedNamesMap: []
+        }
+        this.owning.clear()
+
+        this.sendData = false;
+    }
+  
+    setColor(color) {
+        this.gameData.color = color
+        this.cells.forEach((cell) => {
+            cell.color = color
+        })
+
+    }
+    setName(name) {
+        this.gameData.name = name
+        this.cells.forEach((cell) => {
+            cell.name = name
+            cell.updCode()
+        })
+
+
+    }
+
+    removeCell(cell) {
+        this.cells.delete(cell.id)
+        this.cellHash[cell.id] = false;
+        if (this.cells.size == 0) this.onDeath(cell.killer)
+    }
+ 
+
+    getBiggest() {
+        var cell = false;
+        this.cells.forEach((c) => {
+            if (!cell || c.mass > cell.mass) cell = c
+        })
+        return cell;
+    }
+    getScore(re) {
+
+        if (re) {
+            var l = 0;
+            this.cells.forEach((n) => {
+                l += n.mass;
+            })
+            this.mass = l;
+            this.score = Math.max(this.score, l)
+            return l
+        }
+        this.score = Math.max(this.score, this.mass)
+        return this.score
+    }
+    deleteNodes(main) {
+
+        this.socket.sendDelete(main.deleteR)
+    }
+}
