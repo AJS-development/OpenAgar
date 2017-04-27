193d976320f821cc424e2375a5f56fab49d4ef17
Andrew S <andrews54757@gmail.com>
2017-04-27 10:43:11 -0400

    Update controller.js

--- source/core/controller.js
+++ source/core/controller.js
@@ -32,7 +32,7 @@ Sounds.add('start', 'start.mp3')
 Sounds.add('alert', 'alert.mp3')
 Sounds.add('tone', 'tone.mp3')
 
-_version = "4.4.0"
+_version = "4.5.0"
 _key = ""
 exit = function (a) {
     process.exit(a)
